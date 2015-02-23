77f723e2d96daae7a82e776590ef014c971b9cef
chloewolf <chwo0667@colorado.edu>
2015-02-23 21:30:32 +0000

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
- resources :articles
+ resources :articles do
+  resources :comments
+end
+ 
+end
+Blogger::Application.routes.draw do
+
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
+  resources :tags
+
 end
